e305ceac5da077885a6c71c5f511f113deb63b86
mannubhai1 <manurbhav16@gmail.com>
2024-07-01 10:55:15 +0000

    my new nada program

--- programs/my_first_program.py
+++ programs/my_first_program.py
@@ -1,12 +1,17 @@
 from nada_dsl import *
 
-
 def nada_main():
     party1 = Party(name="Party1")
-    my_int1 = SecretInteger(Input(name="my_int1", party=party1))
-    my_int2 = SecretInteger(Input(name="my_int2", party=party1))
+    height_m = SecretInteger(Input(name="my_int1", party=party1))
+    weight_kg = SecretInteger(Input(name="my_int2", party=party1))
 
     # write the computation for your program here - use my_int1 and my_int2 as inputs
     # make sure you change the output below to be your new output
 
-    return [Output(my_int1, "my_output", party1)]
+    height_squared = height_m * height_m
+    bmi = weight_kg / height_squared
+    
+    # return bmi
+  
+
+    return [Output(bmi, "my_output", party1)]
